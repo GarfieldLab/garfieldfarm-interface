1c849138abddf2c9c91008886872d33cc6f346c0
root <garfieldfinance@outlook.com>
2021-05-24 08:18:56 +0000

    Updates

---
+++ precache-manifest.f0075ef4e9f7927a6cc7f03a619cd0fc.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b33f111c8ce5fef2c78962d954b0f39f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c4fcb6eb172a73f3d59",
+    "url": "/static/css/main.5f656814.chunk.css"
+  },
+  {
+    "revision": "1048406509a8d736f957",
+    "url": "/static/js/0.df338408.chunk.js"
+  },
+  {
+    "revision": "634eb3430effbcec5fe4",
+    "url": "/static/js/10.b8ce0bdf.chunk.js"
+  },
+  {
+    "revision": "f1dd16e0050105a6992c",
+    "url": "/static/js/3.4edbf3ec.chunk.js"
+  },
+  {
+    "revision": "2ea0bd56bca17deb6aeb434005a4f09a",
+    "url": "/static/js/3.4edbf3ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e609119ae795b6811309",
+    "url": "/static/js/4.9d7e050c.chunk.js"
+  },
+  {
+    "revision": "4440e317060694765aff",
+    "url": "/static/js/5.13eac2b1.chunk.js"
+  },
+  {
+    "revision": "800c8c88b71490b35d9e636bbf8c583d",
+    "url": "/static/js/5.13eac2b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c250ec7b9f2c5ed373c",
+    "url": "/static/js/6.7727280d.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/static/js/6.7727280d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "278765628aca65f58d04",
+    "url": "/static/js/7.5bd018ed.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/7.5bd018ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c463d4bef1a38eca3c9c",
+    "url": "/static/js/8.9eab3bd7.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/static/js/8.9eab3bd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eb5b56c3862d2bedeee",
+    "url": "/static/js/9.7a7aab3f.chunk.js"
+  },
+  {
+    "revision": "6c4fcb6eb172a73f3d59",
+    "url": "/static/js/main.66eccb9d.chunk.js"
+  },
+  {
+    "revision": "a1d49114ba6ddb4a2785",
+    "url": "/static/js/runtime-main.c4ec0dd9.js"
+  },
+  {
+    "revision": "15104bece2e253e99445b69a639620fb",
+    "url": "/static/media/BETH.15104bec.svg"
+  },
+  {
+    "revision": "1d38fc66aef967257a27c3be0ea76b01",
+    "url": "/static/media/GFT.1d38fc66.svg"
+  },
+  {
+    "revision": "734cfd8e2613cfd3390873cf70b3d87b",
+    "url": "/static/media/LINK.734cfd8e.svg"
+  },
+  {
+    "revision": "8e72f6f2edbc451e3bba41e50d2b8fcd",
+    "url": "/static/media/MDX.8e72f6f2.png"
+  },
+  {
+    "revision": "e2b742d6f5fd75d573863c11fce06fd7",
+    "url": "/static/media/UNI.e2b742d6.svg"
+  },
+  {
+    "revision": "839a0f0d6e0a69fed8d7846344d2100b",
+    "url": "/static/media/USDT.839a0f0d.png"
+  },
+  {
+    "revision": "68ae2e94bcc33b092d0e912ab40e4f88",
+    "url": "/static/media/Vault.68ae2e94.png"
+  },
+  {
+    "revision": "b2b62f02e6053e8e3daf2b95df67e08b",
+    "url": "/static/media/YFI.b2b62f02.svg"
+  },
+  {
+    "revision": "efe96d33fd02dc32a432a32531aaae9c",
+    "url": "/static/media/background_2.efe96d33.jpg"
+  },
+  {
+    "revision": "1598c7b9f3949d256ff549603e745dbe",
+    "url": "/static/media/sidebar.1598c7b9.jpg"
+  }
+]);
