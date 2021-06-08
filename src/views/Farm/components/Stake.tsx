8c08cd0e59c12c351e2c23b4f88e620978e5ecd1
root <garfieldfinance@outlook.com>
2021-06-08 06:21:29 +0000

    update

--- src/views/Farm/components/Stake.tsx
+++ src/views/Farm/components/Stake.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import Button from '../../../components/Button';
 import Card from '../../../components/Card';
 import CardContent from '../../../components/CardContent';
 import CardIcon from '../../../components/CardIcon';
+import { AddIcon, RemoveIcon } from '../../../components/icons';
+import IconButton from '../../../components/IconButton';
 import Label from '../../../components/Label';
 import Value from '../../../components/Value';
 
-// import useApprove from '../../../hooks/useApprove';
-// import useModal from '../../../hooks/useModal';
-// import useStake from '../../../hooks/useStake';
+import useApprove, { ApprovalState } from '../../../hooks/useApprove';
+import useModal from '../../../hooks/useModal';
+import useStake from '../../../hooks/useStake';
 import useStakedBalance from '../../../hooks/useStakedBalance';
-// import useTokenBalance from '../../../hooks/useTokenBalance';
-// import useWithdraw from '../../../hooks/useWithdraw';
+import useTokenBalance from '../../../hooks/useTokenBalance';
+import useWithdraw from '../../../hooks/useWithdraw';
 
 import { getDisplayBalance } from '../../../utils/formatBalance';
 
-// import DepositModal from './DepositModal';
-// import WithdrawModal from './WithdrawModal';
+import DepositModal from './DepositModal';
+import WithdrawModal from './WithdrawModal';
 import TokenSymbol from '../../../components/TokenSymbol';
 import { Farm } from '../../../go-farm';
 
@@ -26,36 +29,38 @@ interface StakeProps {
 }
 
 const Stake: React.FC<StakeProps> = ({ farm }) => {
-  
-  // const tokenBalance = useTokenBalance(farm.depositToken);
+  const [approveStatus, approve] = useApprove(farm.depositToken, farm.address);
+
+  // TODO: reactive update of token balance
+  const tokenBalance = useTokenBalance(farm.depositToken);
   const stakedBalance = useStakedBalance(farm.pid);
 
-  // const { onStake } = useStake(farm);
-  // const { onWithdraw } = useWithdraw(farm);
+  const { onStake } = useStake(farm);
+  const { onWithdraw } = useWithdraw(farm);
 
-  // const [onPresentDeposit, onDismissDeposit] = useModal(
-  //   <DepositModal
-  //     max={tokenBalance}
-  //     decimals={farm.depositToken.decimal}
-  //     onConfirm={(amount) => {
-  //       onStake(amount);
-  //       onDismissDeposit();
-  //     }}
-  //     tokenName={farm.depositTokenName}
-  //   />,
-  // );
+  const [onPresentDeposit, onDismissDeposit] = useModal(
+    <DepositModal
+      max={tokenBalance}
+      decimals={farm.depositToken.decimal}
+      onConfirm={(amount) => {
+        onStake(amount);
+        onDismissDeposit();
+      }}
+      tokenName={farm.depositTokenName}
+    />,
+  );
 
-  // const [onPresentWithdraw, onDismissWithdraw] = useModal(
-  //   <WithdrawModal
-  //     max={stakedBalance}
-  //     decimals={farm.depositToken.decimal}
-  //     onConfirm={(amount) => {
-  //       onWithdraw(amount);
-  //       onDismissWithdraw();
-  //     }}
-  //     tokenName={farm.depositTokenName}
-  //   />,
-  // );
+  const [onPresentWithdraw, onDismissWithdraw] = useModal(
+    <WithdrawModal
+      max={stakedBalance}
+      decimals={farm.depositToken.decimal}
+      onConfirm={(amount) => {
+        onWithdraw(amount);
+        onDismissWithdraw();
+      }}
+      tokenName={farm.depositTokenName}
+    />,
+  );
 
   return (
     <Card>
@@ -75,7 +80,7 @@ const Stake: React.FC<StakeProps> = ({ farm }) => {
             <Value value={getDisplayBalance(stakedBalance, farm.depositToken.decimal)} />
             <Label text={`质押的${farm.depositTokenName}`} />
           </StyledCardHeader>
-          {/* <StyledCardActions>
+          <StyledCardActions>
             {approveStatus !== ApprovalState.APPROVED ? (
               <Button
                 disabled={
@@ -99,7 +104,7 @@ const Stake: React.FC<StakeProps> = ({ farm }) => {
                 </IconButton>
               </>
             )}
-          </StyledCardActions> */}
+          </StyledCardActions>
         </StyledCardContentInner>
       </CardContent>
     </Card>
@@ -114,6 +119,17 @@ const StyledCardHeader = styled.div`
   align-items: center;
   display: flex;
   flex-direction: column;
+`;
+const StyledCardActions = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: ${(props) => props.theme.spacing[6]}px;
+  width: 100%;
+`;
+
+const StyledActionSpacer = styled.div`
+  height: ${(props) => props.theme.spacing[4]}px;
+  width: ${(props) => props.theme.spacing[4]}px;
 `;
 
 const StyledCardContentInner = styled.div`
