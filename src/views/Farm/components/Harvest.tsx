528644c98b23a2ce126a4968f64a98d688dc830d
root <garfieldfinance@outlook.com>
2021-06-22 07:10:09 +0000

    update

--- src/views/Farm/components/Harvest.tsx
+++ src/views/Farm/components/Harvest.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import Button from '../../../components/Button';
 import Card from '../../../components/Card';
 import CardContent from '../../../components/CardContent';
 import CardIcon from '../../../components/CardIcon';
@@ -8,6 +8,7 @@ import Label from '../../../components/Label';
 import Value from '../../../components/Value';
 import useEarnings from '../../../hooks/useEarnings';
 import { getDisplayBalance } from '../../../utils/formatBalance';
+import useHarvest from '../../../hooks/useHarvest';
 
 import TokenSymbol from '../../../components/TokenSymbol';
 import { Farm } from '../../../go-farm';
@@ -18,7 +19,7 @@ interface HarvestProps {
 
 const Harvest: React.FC<HarvestProps> = ({ farm }) => {
   const earnings = useEarnings(farm.pid);
-
+  const { onReward } = useHarvest(farm);
   return (
     <Card>
       <CardContent>
@@ -31,15 +32,20 @@ const Harvest: React.FC<HarvestProps> = ({ farm }) => {
 
             <Label text={`赚到的${farm.earnTokenName}`} />
           </StyledCardHeader>
-          {/* <StyledCardActions>
+          <StyledCardActions>
             <Button onClick={onReward} disabled={earnings.eq(0)} text="收获"  />
-          </StyledCardActions> */}
+          </StyledCardActions>
         </StyledCardContentInner>
       </CardContent>
     </Card>
   );
 };
-
+const StyledCardActions = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: ${(props) => props.theme.spacing[6]}px;
+  width: 100%;
+`;
 const StyledCardHeader = styled.div`
   align-items: center;
   display: flex;
